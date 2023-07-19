5460ebb077af3debe90f1f2e3ce0ae51cc31e513
hysts <hysts@users.noreply.huggingface.co>
2023-07-19 12:07:48 +0000

    Update

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ def get_prompt(message: str, chat_history: list[tuple[str, str]],
                system_prompt: str) -> str:
     texts = [f'[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n']
     for user_input, response in chat_history:
-        texts.append(f'{user_input} [/INST] {response} </s><s> [INST] ')
+        texts.append(f'{user_input.strip()} [/INST] {response.strip()} </s><s> [INST] ')
     texts.append(f'{message.strip()} [/INST]')
     return ''.join(texts)
 
