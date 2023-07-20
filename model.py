dc8c56e56fcc78516408d158e8ffc724fad86b30
hysts <hysts@users.noreply.huggingface.co>
2023-07-20 06:41:08 +0000

    Update

--- model.py
+++ model.py
@@ -34,7 +34,7 @@ def run(message: str,
         top_p: float = 0.95,
         top_k: int = 50) -> Iterator[str]:
     prompt = get_prompt(message, chat_history, system_prompt)
-    inputs = tokenizer([prompt], return_tensors='pt').to("cuda")
+    inputs = tokenizer([prompt], return_tensors='pt').to('cuda')
 
     streamer = TextIteratorStreamer(tokenizer,
                                     timeout=10.,
